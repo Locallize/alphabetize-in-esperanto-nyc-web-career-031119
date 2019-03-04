9fa84d0696962dfc474cfb8d0629772d0308512a
Fillan Lee <fillanlee@aol.com>
2019-03-04 21:52:50 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
 def alphabetize(arr)
-  # code here
+  
+  # ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  each_letter = alphabet.split("")
+  
+counter = 0  
+
+  arr.sort_by do |words| 
+    
+    each_letter
+  # if each_letter.index(words[0]) > each_letter.index(words[counter+1])
+     
+  #   end
+    end
 end
